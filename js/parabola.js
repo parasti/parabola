22494b3dc404101ab0818a63c61fa376a35680e3
Jānis Rūcis <parasti@gmail.com>
2017-06-08 00:01:31 +0300

    Realize we don't need a standalone bundle

--- js/parabola.js
+++ js/parabola.js
@@ -1,3 +1,3 @@
-// browserify --standalone Parabola parabola.js
-// beefy js/parabola.js:parabola.js --live -- --standalone Parabola
-module.exports = require('./main.js');
+var init = require('./main.js');
+
+document.addEventListener('DOMContentLoaded', init);
